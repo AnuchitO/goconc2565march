c7c551d2d7bb63497c861a1479b4b951081c87d9
AnuchitO <anuchit.prasertasng@gmail.com>
2022-03-25 11:33:12 +0700

    workshop: send data over channel

---
+++ hey/hey_chan.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func hey(msg string) {
+	for i := 0; ; i++ {
+		s := fmt.Sprintf("hey %s Gopher %d", msg, i)
+		fmt.Println(s)
+		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+	}
+}
+
+func main() {
+	go hey("penguin")
+
+	fmt.Scanln()
+	fmt.Println("Done")
+}
